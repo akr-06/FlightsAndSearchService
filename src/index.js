0486053884f195e31c45c4221c36a2db8673257d
Amit Kumar Roy <akumarroy86@gmail.com>
2022-12-13 14:39:27 +0530

    Added sequelize package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const { PORT } = require('./config/serverConfig')
+const { PORT } = require('./config/serverConfig');
 
+app.use(express.json());
 
 const setupAndStartServer = async () => {
     app.listen(PORT,()=> {
         console.log(`server started at ${PORT}`);
     })
-}
+};
 
-setupAndStartServer()
+setupAndStartServer();
