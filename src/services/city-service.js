b77efe1ed97372e3fe25d2070405d28c4951420a
Amit Kumar Roy <akumarroy86@gmail.com>
2022-12-17 09:23:48 +0530

    Added city service

---
+++ src/services/city-service.js
@@ -0,0 +1,34 @@
+const { CityRepository } = require('../repository/index');
+
+class CityService {
+    constructor(){
+        this.cityRepository = new CityRepository();
+    }
+
+    async createCity(data){
+        try {
+            const city = await this.cityRepository.createCity(data);
+            return city;
+        } catch (error) {
+            throw {error};
+        }
+    }
+
+    async deleteCity(cityId){
+        try {
+            const response = await this.cityRepository.deleteCity(cityId);
+            return response;
+        } catch (error) {
+            throw {error};
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city = await this.cityRepository.getCity(cityId);
+            return city;
+        } catch (error) {
+            throw {error};
+        }
+    }
+}
