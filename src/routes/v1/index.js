8a116025927d161efcc4c442dc30112082096e1b
Amit Kumar Roy <akumarroy86@gmail.com>
2022-12-21 17:57:06 +0530

    Added middleware to validate incoming request for creating Flight

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const { FlightMiddlewares }= require('../../middlewares/index');
+
 const req = require('express/lib/request');
 const CityController = require('../../controllers/city-controller');
 const FlightController = require('../../controllers/flight-controller');
-const AirportController = require('../../controllers/airport-controller')
+const AirportController = require('../../controllers/airport-controller');
+
 
 const router = express.Router();
 
-//router.post('/city',cityController.create);
 router.route('/city').post(CityController.create).get(CityController.getAll);
 router.route('/city/:id').delete(CityController.destroy).patch(CityController.update).get(CityController.get);
 
-router.route('/flights').post(FlightController.create).get(FlightController.getAll);
+router.route('/flights').post(FlightMiddlewares.validateCreateFlight,FlightController.create).get(FlightController.getAll);
 
 router.route('/airports').post(AirportController.create);
 
